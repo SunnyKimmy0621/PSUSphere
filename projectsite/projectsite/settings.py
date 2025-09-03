4e6ed7e8044dcbd4f6093ec9983c3722d8f44bf5
Kim Padrones <kimmyonceuuyt@gmail.com>
2025-09-03 17:41:57 +0800

    readme hotfix

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -114,7 +114,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static irles (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
